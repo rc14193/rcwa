109202d7fe9b6eff99758e5c781e816ce2817a74
foglet15 <emdort@gmail.com>
2022-07-31 16:23:49 -0400

    add charts and setup plotting

--- rcwa_www/frontend_rcwa/src/App.js
+++ rcwa_www/frontend_rcwa/src/App.js
@@ -3,14 +3,35 @@ import * as C from "./constants"
 import Banner from './components/Banner';
 import GraphDisplay from './components/GraphDisplay';
 import ControlColumn from './components/ControlColumn';
+import { useState } from 'react';
 
 function App() {
+
+  const [RTot, setRTot] = useState([])
+  const [TTot, setTTot] = useState([])
+  const [wavelengths, setWavelengths] = useState([])
+
+  if(wavelengths.length == 0){
+    var wave = []
+    var r = []
+    var t = []
+    for(let i = 0; i < 200; i++){
+      wave.push(200+i)
+      r.push(30)
+      t.push(40)
+    }
+    setRTot(r)
+    setTTot(t)
+    setWavelengths(wave)
+
+  }
+
   return (
     <div className='mainContentGroup'>
       <Banner />
       <div className='displayArea'>
-        <GraphDisplay />
-        <ControlColumn />
+        <GraphDisplay RTot={RTot} TTot={TTot} wavelengths={wavelengths}/>
+        <ControlColumn setTTot={setTTot} setRTot={setRTot} setWavelengths={setWavelengths}/>
       </div>
       <footer>
         <div>
--- rcwa_www/frontend_rcwa/src/components/GraphDisplay.js
+++ rcwa_www/frontend_rcwa/src/components/GraphDisplay.js
@@ -1,8 +1,24 @@
 
-export default function GraphDisplay(){
+import { LineChart, Line, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Tooltip } from 'recharts';
+
+export default function GraphDisplay({RTot, TTot, wavelengths}){
+    var data = []
+    for(let i = 0; i < wavelengths.length; i++){
+        data.push({"RTot": RTot[i], "TTot": TTot[i], "wavelengths": wavelengths[i]})
+    }
+    console.log(data)
     return(
         <div className="graphDisplay">
         Graph Display
+        <ResponsiveContainer width="100%" height="90%">
+            <LineChart style={{color: 'black'}} data={data}>
+                <XAxis dataKey="wavelengths"/>
+                <YAxis domain={[0, 100]}/>
+                <Tooltip />
+                <Line dataKey="RTot"/>
+                <Line dataKey="TTot"/>
+            </LineChart>
+        </ResponsiveContainer>
         </div>
     )
 }
