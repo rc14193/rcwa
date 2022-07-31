96d4b42a7cf6fa57326a9e3376880116a7167f27
foglet15 <emdort@gmail.com>
2022-07-31 16:59:22 -0400

    remove wireframe for graph

--- rcwa_www/frontend_rcwa/src/components/GraphDisplay.js
+++ rcwa_www/frontend_rcwa/src/components/GraphDisplay.js
@@ -9,18 +9,18 @@ export default function GraphDisplay({RTot, TTot, wavelengths}){
     console.log(data)
     return(
         <div className="graphDisplay">
-        <ResponsiveContainer width="100%" height="95%">
-            <LineChart style={{color: 'black'}} data={data}
-                margin={{ top: 20, right: 30, left: 20, bottom: 10}}>
-                <XAxis dataKey="wavelengths" tick={{fill: 'white', stroke: 'white'}} stroke="white" 
-                    label={{fill:'white', value: "Wavelength", position:"insideBottom", offset: -5}}/>
-                <YAxis domain={[0, 100]} tick={{fill: 'white', stroke: 'white'}} stroke="white" 
-                label={{fill:'white', value: "Efficiency", angle:-90, position:"insideLeft"}}/>
-                <Tooltip />
-                <Line dataKey="RTot" dot={false}/>
-                <Line dataKey="TTot" dot={false}/>
-            </LineChart>
-        </ResponsiveContainer>
+            <ResponsiveContainer width="100%" height="95%">
+                <LineChart data={data}
+                    margin={{ top: 20, right: 30, left: 20, bottom: 10}}>
+                    <XAxis dataKey="wavelengths" tick={{fill: 'white', stroke: 'white'}} stroke="white" 
+                        label={{fill:'white', value: "Wavelength", position:"bottom", offset: -5}}/>
+                    <YAxis domain={[0, 100]} tick={{fill: 'white', stroke: 'white'}} stroke="white" 
+                    label={{fill:'white', value: "Efficiency", angle:-90, position:"insideLeft"}}/>
+                    <Tooltip />
+                    <Line dataKey="RTot" dot={false}/>
+                    <Line dataKey="TTot" dot={false}/>
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     )
 }
